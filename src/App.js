fbc8ff97834fdbf6f980728a76bc858dfa0003ed
Derian Castro <deriancastro@hotmail.com>
2021-05-13 17:12:15 +0200

    Add darkmodus

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import { useState } from 'react'
 import './App.css'
 import Card from './Card'
 import pokemons from './data.json'
 
 export default function App() {
+  const [isDarmode, setIsDarkmode] = useState(false)
+
   return (
     <div className="App">
+      <button onClick={() => setIsDarkmode(!isDarmode)}>Toggle darkmode</button>
       {pokemons.map(pokemon => {
         const { name, images, types, id } = pokemon
         return (
-          <Card key={id} id={id} name={name} images={images} types={types} />
+          <Card
+            key={id}
+            id={id}
+            isDarkmode={isDarmode}
+            name={name}
+            images={images}
+            types={types}
+          />
         )
       })}
     </div>
--- src/Card.js
+++ src/Card.js
@@ -1,20 +1,38 @@
+import React, { useState } from 'react'
 import './Card.css'
 
-export default function Card({ name, images, types, id }) {
+export default function Card({ name, images, types, id, isDarkmode }) {
+  const [isActive, setIsActive] = useState(false)
+  const [showImg, setShowImg] = useState(false)
+
   return (
-    <section className="Card">
+    <section className={isDarkmode ? 'Card Card--darkmode' : 'Card'}>
       <h2>
         {name} @{id}
       </h2>
-      <ul>
-        {types.map(type => (
-          <li key={type}>{type}</li>
-        ))}
-      </ul>
-      <div>
-        <img src={images.front} alt="" />
-        <img src={images.back} alt="" />
-      </div>
+      <button onClick={() => setIsActive(!isActive)}>
+        {isActive ? 'Hide' : 'show'}
+      </button>
+      {isActive && (
+        <>
+          <ul>
+            {types.map(type => (
+              <li key={type}>{type}</li>
+            ))}
+          </ul>
+          <button onClick={() => setShowImg(!showImg)}>
+            {showImg ? 'Hide photos' : 'Show photos'}
+          </button>
+          <div>
+            {showImg && (
+              <>
+                <img src={images.front} alt="" />
+                <img src={images.back} alt="" />
+              </>
+            )}
+          </div>
+        </>
+      )}
     </section>
   )
 }
